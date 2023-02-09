3a6b7bb35c221db838fd8c47c3ccdb412caa2f5d
tris <nexus4fortune@gmail.com>
2023-02-10 04:56:07 +0900

    設定変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://tris5572.github.io",
-	base: "/astro-tutrial",
+	site: 'https://tris5572.github.io',
+	base: '/astro-tutorial',
 });
