d7c92afd8f473ed33590d08e32b9d88ea76d6566
tris <nexus4fortune@gmail.com>
2023-02-10 04:48:08 +0900

    GitHub Actionsによるデプロイを設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: "https://tris5572.github.io",
+	base: "/astro-tutrial",
+});
