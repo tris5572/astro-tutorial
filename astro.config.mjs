6b8e398bd6880dee67e41072a448f12afce152ff
tris <nexus4fortune@gmail.com>
2023-02-11 16:56:19 +0900

    Tutorial finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://tris5572.github.io',
-	base: '/astro-tutorial',
+  site: 'https://tris5572.github.io',
+  base: '/astro-tutorial',
+  integrations: [preact()]
 });
